a83223ba346dc5d828cc511bd90e778506691f77
JochiSt <jochen.steinmann@web.de>
2022-03-07 12:09:01 +0100

    added message listener
    just a simple print listener

--- PeakTech_3315/test_3315.py
+++ PeakTech_3315/test_3315.py
@@ -3,13 +3,17 @@
 import time
 from PeakTech_DMM3315 import PeakTech_DMM3315
 
+def printMeasurement(value, unit):
+    print(value, unit)
+
 mm = PeakTech_DMM3315()
 
 raw = b'001996002\r\n'
 print(raw)
 print( mm.parseString(raw) )
 
-#mm.connect("COM9")
-#time.sleep(5)
-#mm.disconnect()
+mm.connect("COM9")
+mm.add_message_listener(printMeasurement)
+time.sleep(5)
+mm.disconnect()
 
